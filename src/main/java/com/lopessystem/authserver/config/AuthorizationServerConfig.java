b59e315f94334899677737e704f41162aef5dd02
Anderson Lopes <>
2023-09-18 22:06:33 +0200

    Configure introspection endpoint

--- src/main/java/com/lopessystem/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/lopessystem/authserver/config/AuthorizationServerConfig.java
@@ -1,10 +1,49 @@
 package com.lopessystem.authserver.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("clinic-api")
+                .secret(passwordEncoder.encode("api-123"))
+                .authorizedGrantTypes("password")
+                .scopes("write", "read")
+                .accessTokenValiditySeconds(60 * 60 * 6) // 6 hours
+                .and()
+                .withClient("web-app")
+                .secret(passwordEncoder.encode("web-123"))
+                .authorizedGrantTypes("basic")
+                .scopes("write", "read");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // security.checkTokenAccess("isAuthenticated()");
+        security.checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
