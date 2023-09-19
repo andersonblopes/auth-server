cc104d3eb31609f7196cd8c9af51f4aa74117b44
Anderson Lopes <>
2023-09-19 09:13:08 +0200

    Implements refresh token

--- src/main/java/com/lopessystem/authserver/config/AuthorizationServerConfig.java
+++ src/main/java/com/lopessystem/authserver/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.lopessystem.authserver.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients
                 .inMemory()
                 .withClient("clinic-api")
                 .secret(passwordEncoder.encode("api-123"))
-                .authorizedGrantTypes("password")
+                .authorizedGrantTypes("password", "refresh_token")
                 .scopes("write", "read")
-                .accessTokenValiditySeconds(60 * 60 * 6) // 6 hours
+                .accessTokenValiditySeconds(60 * 5) // 5 minutes
+                .refreshTokenValiditySeconds(60 * 60 * 5) // 5 hours
                 .and()
                 .withClient("checkToken")
                 .secret(passwordEncoder.encode("check123"))
@@ -48,5 +53,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints.authenticationManager(authenticationManager);
+        endpoints.userDetailsService(userDetailsService);
+        // endpoints.reuseRefreshTokens(false);
     }
 }
